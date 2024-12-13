9557d2e2636264e35cf0b06801f9495dab75fd68
Aman Mourya <110944383+Amanmourya09@users.noreply.github.com>
2024-12-13 22:35:03 +0530

    Time: 258 ms (25.63%), Space: 150.5 MB (18.95%) - LeetHub

---
+++ 2695-find-score-of-an-array-after-marking-all-elements/2695-find-score-of-an-array-after-marking-all-elements.cpp
@@ -0,0 +1,30 @@
+class Solution {
+    #define l1 long long
+    #define pii pair<int, int>
+public:
+    long long findScore(vector<int>& nums) {
+        int n = nums.size();
+        priority_queue<pii,vector<pii>,greater<pii>> minheap;
+        for(int i =0; i<n; ++i)
+        minheap.push(make_pair(nums[i],i));
+
+        unordered_set<int> marked_index;
+        l1 sum = 0;
+        while(!minheap.empty()){
+            pii curr = minheap.top();
+            minheap.pop();
+
+            int val = curr.first;
+            int index = curr.second;
+            if(marked_index.count(index))
+            continue;
+
+            sum += val;
+            marked_index.insert(index);
+            marked_index.insert(index-1);
+            marked_index.insert(index+1);
+        }
+        return sum;
+        
+    }
+};
